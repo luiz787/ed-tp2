eb6aadb1959961ddc853e06596be58238581bfc7
luiz787 <diasbertoluiz@gmail.com>
2019-06-08 18:16:29 -0300

    Altera ordem dos testes

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ make
 declare -a quickSortTypes=(
     "QC"
     "QM3"
-    "QPE"
     "QI1"
     "QI5"
     "QI10"
     "QNR"
+    "QPE"
 )
 
 declare -a arrayTypes=(
@@ -22,9 +22,9 @@ declare -a arrayTypes=(
 dir="output/"
 underscore="_"
 
-for arrSize in {50000..500000..50000}; do
-    for quickSort in "${quickSortTypes[@]}"; do
-        for arrType in "${arrayTypes[@]}"; do
+for quickSort in "${quickSortTypes[@]}"; do
+    for arrType in "${arrayTypes[@]}"; do
+        for arrSize in {50000..500000..50000}; do
             for iteration in {1..20}; do
                 echo "Running $quickSort for array size $arrSize with type $arrType, iteration $iteration".
                 ./tp2 $quickSort $arrType $arrSize >> "$dir$quickSort$underscore$arrType$underscore$arrSize.txt"
